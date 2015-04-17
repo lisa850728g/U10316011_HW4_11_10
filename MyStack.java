320cc068bbd9afc3bd2ab70b3c7525c235b6e69e
lisa850728g <lisa850728g@gmail.com>
2015-04-17 14:19:40 +0800

    change MyStack.java
    1.增加註解
    2.補上ArrayList的型態
    3.更改回傳值

--- MyStack.java
+++ MyStack.java
@@ -1,25 +1,34 @@
-public class MyStack extends ArrayList{
+import java.util.*;
+
+/**MyStack繼承ArrayList，型態為Object*/
+public class MyStack extends ArrayList<Object>{
 	
+	//Main Method
 	public static void main(String[] args) {
 	
+	//陣列是否為空值
 	public boolean isEmpty(){
 		return super.isEmpty();
 	}
 	
+	//陣列大小
 	public int getSize(){
 		return super.getSize();
 	}
 	
+	//取得陣列元素中的東西
 	public Object peek(){
 		return  super.get(getSize()-1);
 	}
 
+	//assign陣列元素中的東西進入o，然後移除那格陣列元素
 	public Object pop(){
 		Object o = super.get(getSize()-1);
 		super.remove(getSize()-1);
-		return 0;
+		return o;
 	}
 	
+	//傳值進入ArrayList
 	public void push(Object o){
 		super.add(o);
 	}
